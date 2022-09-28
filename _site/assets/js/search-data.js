ec193a0dfae57216a76066fe3e0ea0445334c3c9
jiminjung <jimin606@naver.com>
2022-09-28 15:35:32 +0900

    layout changed

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/"
+      },{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/chocolate-chip-cookies/"
+      },{
+        "title": "Oatmeal Cookies",
+        "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/oatmeal-cookies/"
+      },{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/peanut-butter-cookies/"
+      }]
---
+++ recipes.md
@@ -0,0 +1,9 @@
+---
+title: Holly
+layout: collection
+permalink: /
+collection: recipes
+entries_layout: grid
+---
+
+Sample document listing for the collection `_recipes`.
